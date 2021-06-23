2d45c93f290096803a4a2597a2e1820e840c2f20
KiranDixit <77818577+KiranDixit@users.noreply.github.com>
2021-06-23 10:54:10 +0530

    Add files via upload
    Added java programs

---
+++ input.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class input {
+	public static void main(String args[]) {
+		int a=10;
+		System.out.println(a);
+		Scanner s= new Scanner(System.in);
+		System.out.println("Enter");
+		int b=s.nextInt();
+		System.out.println(b);
+	}
+
+}
---
+++ pract.java
@@ -0,0 +1,9 @@
+
+public class pract {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
